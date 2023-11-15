4029dccd5a0f745f5799515c6bbdfd5c015f774d
Avro Podder <104518526+av23ro@users.noreply.github.com>
2023-11-15 21:11:18 +0530

    Add files via upload

--- linkedlist.c
+++ linkedlist.c
@@ -11,7 +11,7 @@ node *insert_beg(node *head)
 {
     node *temp = head;
     head = (node *)malloc(sizeof(node));
-    printf("Enter the data you want to enter: ");
+    printf("Enter the value you want to enter: ");
     scanf("%d", &head->data);
     head->next = temp;
     return head;
@@ -32,7 +32,7 @@ node *insert_end(node *head)
         }
         temp->next = (node *)malloc(sizeof(node));
         temp = temp->next;
-        printf("Enter the data to be inserted at the end: ");
+        printf("Enter the value you want to enter at end: ");
         scanf("%d", &temp->data);
         temp->next = NULL;
     }
@@ -44,7 +44,7 @@ node *insert_after(node *head)
     node *temp, *temp1;
     temp = head;
     int val;
-    printf("Enter the value after which you want to enter new node: ");
+    printf("Enter the value after which you want to insert new node: ");
     scanf("%d", &val);
     while (temp != NULL && temp->data != val)
     {
@@ -53,14 +53,14 @@ node *insert_after(node *head)
     if (temp == NULL)
     {
         printf("Insertion not possible!\n");
-        return head;
     }
     else
     {
-        temp1 = (node *)malloc(sizeof(node));
+        temp1 = temp->next;
+        temp->next = (node *)malloc(sizeof(node));
+        temp = temp->next;
         printf("Enter the value you want to enter: ");
-        scanf("%d", &temp1->data);
-        temp1->next = temp->next;
+        scanf("%d", &temp->data);
         temp->next = temp1;
     }
     return head;
@@ -69,9 +69,9 @@ node *insert_after(node *head)
 node *del_beg(node *head)
 {
     node *temp = head;
-    if (head == NULL)
+    if (temp == NULL)
     {
-        printf("Deletion not possible!!\n");
+        printf("List is empty!\n");
     }
     else
     {
@@ -85,10 +85,11 @@ node *del_beg(node *head)
 node *del_end(node *head)
 {
     node *temp, *prev;
-    temp = prev = head;
-    if (head == NULL)
+    temp = head;
+    prev = NULL;
+    if (temp == NULL)
     {
-        printf("Deletion is not possible!!\n");
+        printf("List is empty!\n");
     }
     else
     {
@@ -97,7 +98,14 @@ node *del_end(node *head)
             prev = temp;
             temp = temp->next;
         }
-        prev->next = NULL;
+        if (head == temp)
+        {
+            head = NULL;
+        }
+        else
+        {
+            prev->next = NULL;
+        }
         printf("Deleted item is %d\n", temp->data);
         free(temp);
     }
@@ -107,41 +115,40 @@ node *del_end(node *head)
 node *del_any(node *head)
 {
     node *temp, *prev;
-    temp = prev = head;
+    temp = head;
+    prev = NULL;
     int val;
-    printf("Enter the value you want to delete: ");
+    printf("Enter the data you want to delete: ");
     scanf("%d", &val);
-
+    if(temp != NULL && temp->data == val)
+    {
+        head = temp->next;
+        printf("Deleted item is %d\n", temp->data);
+        free(temp);
+        return head;
+    }
+    while (temp != NULL && temp->data != val)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
     if (temp == NULL)
     {
-        printf("Deletion not possible!! List is empty.\n");
+        printf("value does not exist!\n");
     }
     else
     {
-        while (temp != NULL && temp->data != val)
-        {
-            prev = temp;
-            temp = temp->next;
-        }
-
-        if (temp == NULL)
-        {
-            printf("Value %d not found. Deletion not possible.\n", val);
-        }
-        else
-        {
-            prev->next = temp->next;
-            printf("Deleted item is %d\n", temp->data);
-            free(temp);
-        }
+        prev->next = temp->next;
+        printf("Deleted item is %d\n", temp->data);
+        free(temp);
     }
     return head;
 }
 
-void display(node *head)
+void traverse(node *head)
 {
     node *temp = head;
-    if (head == NULL)
+    if (temp == NULL)
     {
         printf("List is empty!\n");
     }
@@ -162,8 +169,8 @@ int main()
     int ch;
     while (1)
     {
-        printf("MENU :\n1.INSERT AT BEGINNING\n2.INSERT AT END\n3.INSERT AFTER A VALUE\n4.DELETE AT BEGINING\n5.DELETE AT END\n6.DELETE ANY\n7.DISPLAY\n8.EXIT\n");
-        printf("Enter your choice: ");
+        printf("MENU:\n1.INSERT AT BEGINNING\n2.INSERT AT END\n3.INSERT AFTER\n4.DELETE BEGINNING\n5.DELETE END\n6.DELETE ANY\n7.DISPLAY\n8.EXIT\n");
+        printf("Enter the choice: ");
         scanf("%d", &ch);
         switch (ch)
         {
@@ -186,7 +193,7 @@ int main()
             head = del_any(head);
             break;
         case 7:
-            display(head);
+            traverse(head);
             break;
         case 8:
             exit(0);
